3b236cb6a9bd294d02c3f23ab8f3782d31fe4ce4
Giancarlo-UCSP <giancarlo.anco@ucsp.edu.pe>
2016-03-22 10:02:47 -0500

    Update Ejercicio(1,6.1,7).c

--- Ejercicios-22-03/Ejercicio(1,6.1,7).c
+++ Ejercicios-22-03/Ejercicio(1,6.1,7).c
@@ -4,7 +4,8 @@ El 1.7 pide escribir el programa que imprima el valor de EOF.
 Con este programa se pueden explicar los dos ejercicios. */
 
 main(){
+	int  c;
 	printf ("Ingresa una tecla: ");
-	getchar();
-	printf ("El valor de EOF es: %d", getchar() != EOF);
+	c = getchar();
+	printf ("El valor de EOF es: %d\n", c != EOF);
 }
