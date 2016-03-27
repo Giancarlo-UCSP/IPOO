d10418303b5edb91883b54d7c6f8861afb0389ef
Giancarlo-UCSP <giancarlo.anco@ucsp.edu.pe>
2016-03-27 13:42:20 -0500

    Update 4.c

--- Homework1/4.c
+++ Homework1/4.c
@@ -2,6 +2,7 @@
 
 main(){
 	int n;
+	printf("Ingrese un número: ");
 	scanf("%d",&n);
-	printf("La suma de los %d primeros números es: %d",n,(n*(n+1))/2);
+	printf("La suma de los %d primeros números enteros positivos es: %d",n,(n*(n+1))/2);
 }
