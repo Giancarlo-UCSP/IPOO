452d16e614996910049c8b9924ca182f9aa432c0
Giancarlo-UCSP <giancarlo.anco@ucsp.edu.pe>
2016-03-27 22:11:29 -0500

    Update 3.cpp

--- Homework1/3.cpp
+++ Homework1/3.cpp
@@ -6,11 +6,11 @@ using namespace std;
 
 int main(){
 	int c;
-	printf("\tNUMERO\tCUADRADO\n");
+	printf("\tNúmero\tCuadrado\n");
 	for (int i=0; i<10;i++){
 		cin>>c;
 		system("cls");
-		printf("\tNUMERO\tCUADRADO\n");
+		printf("\tNúmero\tCuadrado\n");
 		cout<<"\t"<<c<<"\t"<<c*c<<endl;
 	}
 }
