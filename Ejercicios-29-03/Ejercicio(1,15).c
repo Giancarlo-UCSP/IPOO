92284b162a6eff1c0cdb2ce95f853d1225bc9cb1
Giancarlo-UCSP <giancarlo.anco@ucsp.edu.pe>
2016-03-31 20:16:12 -0500

    Ejercicio 1.15

---
+++ Ejercicios-29-03/Ejercicio(1,15).c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+float conversor(float f){
+	float c;
+	c=(5.0/9.0)*(f-32.0);
+	return c;
+}
+
+int imprimir(float f, float c){
+	printf("%3.0f \t\t %6.1f \n", f, c);
+}
+
+main()
+{
+	float fahr, celsius;
+	
+	fahr=LOWER;
+	printf("Fahrenheit \t Celsius\n"); 
+	while (fahr<=UPPER){
+		celsius=conversor(fahr);
+		imprimir(fahr,celsius);
+		fahr=fahr+STEP;
+	}
+}
