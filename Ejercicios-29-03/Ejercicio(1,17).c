295a64e9a3e288f54dbe8a10a17023c4a91f1a9a
Giancarlo-UCSP <giancarlo.anco@ucsp.edu.pe>
2016-04-04 23:33:12 -0500

    Ejercicio 1.17

--- Ejercicios-29-03/Ejercicio(1,17).c
+++ Ejercicios-29-03/Ejercicio(1,17).c
@@ -11,7 +11,7 @@ main(){
 	
 	max=0;
 	
-	while((len=_getline(line,MAXLINE))>0){
+	while((len=_getline(line,MAXLINE)) > 0){
 		if (len>max){
 			max=len;
 			copy(longest,line);
@@ -27,7 +27,7 @@ int _getline(char s[], int lim){
 	int c, i;
 	
 	for(i=0; i<lim-1 && (c=getchar())!=EOF && c!='\n';++i)
-		s[i]=c;
+		s[i] = c;
 	if(c=='\n'){
 		s[i]=c;
 		++i;
