030b3e13ebcba04ae5aae4f416405292e23afbe5
Giancarlo-UCSP <giancarlo.anco@ucsp.edu.pe>
2016-04-22 08:55:02 -0500

    Palindromo

--- Ejercicios/Martes19/Palindromo.c
+++ Ejercicios/Martes19/Palindromo.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
-int palindromo(char *p){
-	int i,j;
-	for (i=0, j=strlen(p)-1;i<j;i++,j--){
+void palindromo(char *p){
+	int i,j,c;
+	for (i=strlen(p)-1, j=0 ; i>=0 ; i-- ,j++){
 		if (p[i]==p[j])
-			return 1;
-		else
-			return 0;
+			c++;
 	}
+	if (c==strlen(p))
+		printf("Es palindromo.\n");
+	else
+		printf("No es palindromo.\n");
 }
 
 int main(){
@@ -16,9 +18,5 @@ int main(){
 	char palabra[20];
 	printf("Ingresa la palabra: \n");
 	scanf("%s",palabra);
-	x=palindromo(palabra);
-	if (x==1)
-		printf("Es palindromo.\n");
-	if (x==0)
-		printf("No es palindromo.\n");
+	palindromo(palabra);	
 }
