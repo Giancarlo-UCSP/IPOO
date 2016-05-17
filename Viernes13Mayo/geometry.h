e5a08421b64e5a29f0828c1375b4aca8250c41c7
Giancarlo-UCSP <giancarlo.anco@ucsp.edu.pe>
2016-05-16 23:02:45 -0500

    geometry.h

---
+++ Viernes13Mayo/geometry.h
@@ -0,0 +1,17 @@
+class Point{
+	
+	private:
+	
+		int x, y ;
+	
+	public :
+		
+		Point(int, int);
+		
+		int getX() const;
+		int getY() const;
+		
+		void setX(const int newX);
+		void setY(const int newY);
+		
+};
