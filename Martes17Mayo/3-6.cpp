2bdbeddc23451b54429ae13a3b1790ab30ad8454
Giancarlo-UCSP <giancarlo.anco@ucsp.edu.pe>
2016-05-16 23:08:40 -0500

    3-6.cpp

---
+++ Martes17Mayo/3-6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+const int ARRAY_LEN = 10;
+
+using namespace std;
+
+int main(){
+	
+	int arr[ARRAY_LEN] = {10};
+		
+	int *xPtr = arr, *yPtr = arr + ARRAY_LEN - 1;
+	
+	cout << *xPtr << ' ' << *yPtr ;
+	
+	return 0;
+
+}
