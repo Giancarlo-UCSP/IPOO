b853628e5086379ca5817a02845ac19d4fa89b02
Giancarlo-UCSP <giancarlo.anco@ucsp.edu.pe>
2016-05-16 23:06:36 -0500

    3-2.cpp

---
+++ Martes17Mayo/3-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void printNum(int number){
+	
+	cout<<number; 
+	
+}
+	
+int main () {
+		
+	int number = 35;
+		
+	printNum(number);
+		
+	return 0;
+		
+}
