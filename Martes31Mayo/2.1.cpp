8760828d44c96abf10c14a33b43b9e55f0358aa2
Giancarlo-UCSP <giancarlo.anco@ucsp.edu.pe>
2016-05-31 09:31:36 -0500

    2.1

--- Martes31Mayo/2.1.cpp
+++ Martes31Mayo/2.1.cpp
@@ -1,6 +1,10 @@
-/* Solo pide definir una función. */
+#include <iostream>
 
 template < typename T>
-T min( const T t1 , const T t2) {
+T min(const T t1 , const T t2) {
 	return t1 < t2 ? t1 : t2;
+}
+
+int main(){
+	std::cout<<min<int>(2,4)<<std::endl;
 }
